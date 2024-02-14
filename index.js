d2251f22d5679ef2d45f9198e8f9d9315f6dea94
Quartz16 <meowcatsrock@naver.com>
2024-02-14 14:12:16 -0700

    enabled customization of features of smiley face

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 
 class SmileyConfig {
-    constructor(x, y, eyeHeight, eyePadding, circleRadius, mouthPaddingX, mouthPaddingY, mouthHeight) {
+    constructor(x, y, size, eyeHeight, eyePadding, circleRadius, mouthPaddingX, mouthPaddingY, mouthHeight) {
         this.x = x;
         this.y = y;
+        this.size = size;
         this.eyeHeight = eyeHeight;
         this.eyePadding = eyePadding;
         this.circleRadius = circleRadius;
@@ -66,11 +67,11 @@ function resizeAndDraw() {
 }
 
 function initializeConfig() {
-    currentConfig = new SmileyConfig(0, 0, 40, 20, 80, 30, 8, 28);
+    currentConfig = new SmileyConfig(0, 0, 1, 40, 20, 80, 30, 8, 28);
 }
 
 function createConfig(x, y) {
-    return new SmileyConfig(x, y, currentConfig.eyeHeight, currentConfig.eyePadding, currentConfig.circleRadius, currentConfig.mouthPaddingX, currentConfig.mouthPaddingY, currentConfig.mouthHeight);
+    return new SmileyConfig(x, y, currentConfig.size, currentConfig.eyeHeight, currentConfig.eyePadding, currentConfig.circleRadius, currentConfig.mouthPaddingX, currentConfig.mouthPaddingY, currentConfig.mouthHeight);
 
 }
 
@@ -88,10 +89,10 @@ function drawSmiley(context, x, y, redraw) {
     console.log("x: " + x + "ep: " + ep + "L: " + leftX + "R: " + rightX);
     createLine(context, leftX, y, leftX, y-config.eyeHeight);
     createLine(context, rightX, y, rightX, y-config.eyeHeight);
-    let startX = x-config.mouthPaddingX;
-    let startY = y+config.mouthPaddingY;
-    let endX = x+config.mouthPaddingX;
-    let endY = y+config.mouthPaddingY+config.mouthHeight;
+    let startX = parseFloat(x)-parseFloat(config.mouthPaddingX);
+    let startY = parseFloat(y)+parseFloat(config.mouthPaddingY);
+    let endX = parseFloat(x)+parseFloat(config.mouthPaddingX);
+    let endY = parseFloat(y)+parseFloat(config.mouthPaddingY)+parseFloat(config.mouthHeight);
     createBezierCurve(context, startX, startY, startX, endY, endX, endY, endX, startY);
     createCircle(context, x, y, config.circleRadius);
 
@@ -131,7 +132,76 @@ function maxEyePadding() {
     return (currentConfig.circleRadius / 2) - 2;
 }
 
+function minCircleRadius() {
+    var canvas = document.getElementById("canvas");
+
+    return parseFloat(Math.max(Math.min(canvas.width, canvas.height)/30)) / parseFloat(2.0);
+}
+
+function maxCircleRadius() {
+    var canvas = document.getElementById("canvas");
+    return parseFloat(Math.max(Math.min(canvas.width, canvas.height), 5)) / parseFloat(2.0);
+}
+
+function minMouthPaddingX() {
+    return currentConfig.eyePadding + Math.max(currentConfig.circleRadius/50, 2); 
+}
+
+function maxMouthPaddingX() {
+    return parseFloat(0.9) * parseFloat(currentConfig.circleRadius) - 2; 
+}
+
+function minMouthPaddingY() {
+    return Math.max(currentConfig.circleRadius/50, 2); 
+}
+
+function maxMouthPaddingY() {
+    return currentConfig.circleRadius - currentConfig.mouthHeight - 2; 
+}
+
+
+function minMouthHeight() {
+    return Math.max(currentConfig.circleRadius/30, 2); 
+}
+
+function maxMouthHeight() {
+    return currentConfig.circleRadius - currentConfig.mouthPaddingY - 2; 
+}
+
+
+function minSize() {
+    var minsize = parseFloat(minCircleRadius()) / parseFloat(currentConfig.circleRadius) * parseFloat(currentConfig.size);
+    console.log("MS: " + minsize);
+    return minsize;
+}
+
+function maxSize() {
+    return parseFloat(maxCircleRadius()) / parseFloat(currentConfig.circleRadius) * parseFloat(currentConfig.size);
+}
+
+function changeSize(newSizeString) {
+    //don't want to do anything if the new size is 0, should be larger than that
+    var newSize = parseFloat(newSizeString);
+    console.log(newSize);
+    if (newSize == 0) return;
+    var oldSize = currentConfig.size;
+    var changeRatio = parseFloat(newSize) / parseFloat(oldSize);
+    
+    console.log("RATIO" + changeRatio);
+    //update all config values
+    currentConfig.eyeHeight *= changeRatio;
+    currentConfig.eyePadding *= changeRatio;
+    currentConfig.circleRadius *= changeRatio;
+    currentConfig.mouthPaddingX *= changeRatio;
+    currentConfig.mouthPaddingY *= changeRatio;
+    currentConfig.mouthHeight *= changeRatio;
+
+}
+
 function updateMaxMins() {
+    var size = document.getElementById("sizeRange");
+    size.min = minSize();
+    size.max = maxSize();
     var eyeHeight = document.getElementById("eyeHeightRange");
     eyeHeight.min = minEyeHeight();
     eyeHeight.max = maxEyeHeight();
@@ -139,4 +209,22 @@ function updateMaxMins() {
     var eyePadding = document.getElementById("eyePaddingRange");
     eyePadding.min = minEyePadding();
     eyePadding.max = maxEyePadding();
+
+    var circleRadius = document.getElementById("circleRadiusRange");
+    circleRadius.min = minCircleRadius();
+    circleRadius.max = maxCircleRadius();
+    var mouthPaddingX = document.getElementById("mouthPaddingXRange");
+    mouthPaddingX.min = minMouthPaddingX();
+    mouthPaddingX.max = maxMouthPaddingX();
+
+    var mouthPaddingY = document.getElementById("mouthPaddingYRange");
+    mouthPaddingY.min = minMouthPaddingY();
+    mouthPaddingY.max = maxMouthPaddingY();
+
+
+    var mouthHeight = document.getElementById("mouthHeightRange");
+    mouthHeight.min = minMouthHeight();
+    mouthHeight.max = maxMouthHeight();
+
+
 }
